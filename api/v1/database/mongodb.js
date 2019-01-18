511beedf00f1451276631f78631f43f868cbe0af
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2019-01-18 15:36:13 -0200

    feat: incorporate try catch in database methods

--- api/v1/database/mongodb.js
+++ api/v1/database/mongodb.js
@@ -14,7 +14,6 @@ class MongoDB {
 
 
     connectDatabase(){
-
         Mongoose.connect('mongodb://myuser:mypassword@localhost:27017/wirecard', { useNewUrlParser: true }, function (error) {
             if (!error) {
                 return
@@ -31,160 +30,169 @@ class MongoDB {
 
 
     async definePaymentCardSchema(){
+        try {
+            this.connectDatabase()
+            this._Paymentschema = new Mongoose.Schema({
+                id_client: {
+                    type: Number,
+                    required: true
+                },
+                name_buyer: {
+                    type: String,
+                    required: true
+                },
+                email_buyer: {
+                    type: String,
+                    required: true
+                },
+                cpf_buyer: {
+                    type: String,
+                    required: true
+                },
+                amount_payment: {
+                    type: Number,
+                    required: true
+                },
+                type_payment: {
+                    type: String,
+                    required: true
+                },
+                card_name: {
+                    type: String,
+                    required: true
+                },
+                card_expiration: {
+                    type: Number,
+                    required: true
+                },
+                card_number: {
+                    type: String,
+                    required: true
+                },
+                card_issuer: {
+                    type: String,
+                    required: true
+                },
+                card_cvv: {
+                    type: Number,
+                    required: true
+                },
+                payment_status: {
+                    type: Number,
+                    require: true,
+                    default: 0
+                },
+            })
+            this._modelPayment = Mongoose.model('payment', this._Paymentschema)
+            return true
 
-        this.connectDatabase()
-
-        this._Paymentschema = new Mongoose.Schema({
-            id_client: {
-                type: Number,
-                required: true
-            },
-            name_buyer: {
-                type: String,
-                required: true
-            },
-            email_buyer: {
-                type: String,
-                required: true
-            },
-            cpf_buyer: {
-                type: String,
-                required: true
-            },
-            amount_payment: {
-                type: Number,
-                required: true
-            },
-            type_payment: {
-                type: String,
-                required: true
-            },
-            card_name: {
-                type: String,
-                required: true
-            },
-            card_expiration: {
-                type: Number,
-                required: true
-            },
-            card_number: {
-                type: String,
-                required: true
-            },
-            card_issuer: {
-                type: String,
-                required: true
-            },
-            card_cvv: {
-                type: Number,
-                required: true
-            },
-            payment_status: {
-                type: Number,
-                require: true,
-                default: 0
-            },
-        })
-
-        this._modelPayment = Mongoose.model('payment', this._Paymentschema)
-
-
+        } catch (error) {
+            throw ('Ocorreu um erro ao conectar o banco e definir o Schema do cartão')
+        }
     }
+
+
 
     async definePaymentBoletoSchema(){
+        try {
+            this.connectDatabase()
+            this._Paymentschema = new Mongoose.Schema({
+                id_client: {
+                    type: Number,
+                    required: true
+                },
+                name_buyer: {
+                    type: String,
+                    required: true
+                },
+                email_buyer: {
+                    type: String,
+                    required: true
+                },
+                cpf_buyer: {
+                    type: String,
+                    required: true
+                },
+                amount_payment: {
+                    type: Number,
+                    required: true
+                },
+                type_payment: {
+                    type: String,
+                    required: true
+                },
+                boleto_codebar:{
+                    type: Number,
+                    required: true
+                },
+                payment_status: {
+                    type: Number,
+                    require: true,
+                    default: 0
+                }
+            })
+            this._modelPayment = Mongoose.model('payment', this._Paymentschema)
+            return true
 
-        this.connectDatabase()
-
-        this._Paymentschema = new Mongoose.Schema({
-            id_client: {
-                type: Number,
-                required: true
-            },
-            name_buyer: {
-                type: String,
-                required: true
-            },
-            email_buyer: {
-                type: String,
-                required: true
-            },
-            cpf_buyer: {
-                type: String,
-                required: true
-            },
-            amount_payment: {
-                type: Number,
-                required: true
-            },
-            type_payment: {
-                type: String,
-                required: true
-            },
-            boleto_codebar:{
-                type: Number,
-                required: true
-            },
-            payment_status: {
-                type: Number,
-                require: true,
-                default: 0
-            }
-        })
-
-        this._modelPayment = Mongoose.model('payment', this._Paymentschema)
-
+        } catch (error) {
+            throw ('Ocorreu um problema ao conectar ao banco e definir o Schema do Boleto', error)
+        }
     }
+
+
 
     defineBuyerSchema(){
 
-        this.connectDatabase()
+        try {
+            this.connectDatabase()
+            this._BuyerSchema = new Mongoose.Schema({
+                name_buyer: {
+                    type: String,
+                    require: true
+                },
+                email_buyer: {
+                    type: String,
+                    require: true
+                },
+                cpf_buyer: {
+                    type: String,
+                    require: true
+                }
+            })
+            return true
 
-        this._BuyerSchema = new Mongoose.Schema({
-            name_buyer: {
-                type: String,
-                require: true
-            },
-            email_buyer: {
-                type: String,
-                require: true
-            },
-            cpf_buyer: {
-                type: String,
-                require: true
-            }
-        })
-
+        } catch (error) {
+            throw ('Ocorreu um problema o conectar ao banco e definir o Schema: ', error)
+        }
 
     }
 
 
     async insertPayment(dataPayment){
-
-        let resulInsert = null
-
         switch (dataPayment.type_payment) {
 
-
             case 'card':
-                console.log( 'conteudo a ser inserido: ', dataPayment )
-                this.definePaymentCardSchema()
-                resulInsert = await this._modelPayment.create(dataPayment)
-                break
+                try {
+                    this.definePaymentCardSchema()
+                    await this._modelPayment.create(dataPayment)
+                    return true
+                } catch (error) {
+                    throw ('Ocorreu um erro ao inserir no banco: ', error)
+                }
 
             case 'boleto':
-                this.definePaymentBoletoSchema()
-                resulInsert = await this._modelPayment.create(dataPayment)
-                break
+                try {
+                    this.definePaymentBoletoSchema()
+                    await this._modelPayment.create(dataPayment)
+                    return true
+                } catch (error) {
+                    throw ('Ocorreu um erro ao inserir no banco: ', error)
+                }
 
             default:
-                resulInsert = 'Esse metodo de pagamento não existe'
-                break
+                throw ('Esse metodo de pagamento não existe')
         }
-
-        console.log( 'Retorno do ResultInsert: ', resulInsert )
-        return resulInsert
     }
-
 }
 
 module.exports = MongoDB
