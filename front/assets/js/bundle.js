ba3458988edb65063baaa5a6a30d2b600156e2b4
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2019-01-26 16:14:56 -0200

    feat: implemented get status by cpf and adjusts in bundle.js to update

--- front/assets/js/bundle.js
+++ front/assets/js/bundle.js
@@ -4,6 +4,8 @@
     var boxBuyer = document.querySelector('.box.buyer')
     var boxStatus = document.querySelector('.box.status')
     var statusbox = document.querySelector('.statusResponse')
+    var statustitle = document.querySelector('#textStatus')
+    var statuscodebar = document.querySelector('#textCodebar')
 
     function listener(event, element, callback) {
         document.querySelector(element)
@@ -43,6 +45,8 @@
     function closeResponse() {
         event.stopPropagation()
         statusbox.classList.remove('-active')
+        statustitle.innerHTML = ''
+        statuscodebar.innerHTML = ''
     }
 
     function closePayment() {
@@ -94,6 +98,7 @@
     var statustitle = document.querySelector('#textStatus')
     var statusbox = document.querySelector('.statusResponse')
     var statuscodebar = document.querySelector('#textCodebar')
+    var typePay = null
 
 
     function listener(event, element, callback) {
@@ -108,27 +113,32 @@
 
 
     function sendPayment() {
-        console.log(cpf.value)
+
+        if (typePay === null) {
+            statusbox.classList.add('-active')
+            statustitle.innerHTML = 'Precisa selecionar o tipo de pagamento'
+            return
+        }
+
         var ajax = new XMLHttpRequest()
         ajax.open('POST', 'http://localhost:8002/payment/register')
         ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-        ajax.send(`id_client=32&cpf_buyer=${cpf.value}&amount_payment=${amount.value}&type_payment=${typePayment}&card_name=${card_name.value}&card_number=${card_number.value}&card_expiration=${card_expiration.value}&card_cvv=${card_cvv.value}`)
+        ajax.send(`id_client=32&cpf_buyer=${cpf.value}&amount_payment=${amount.value}&type_payment=${typePay}&card_name=${card_name.value}&card_number=${card_number.value}&card_expiration=${card_expiration.value}&card_cvv=${card_cvv.value}`)
         ajax.onreadystatechange = function () {
             if (ajax.readyState == 4) {
                 var data = JSON.parse(ajax.response)
-                console.log('Resposta: ' + data)
-                console.log('Resposta: ' + data.IsOk)
-                console.log('Resposta: ' + data.codebar)
                 statusbox.classList.add('-active')
                 if (data.IsOk == true ) {
-                    statustitle.innerHTML = 'Operação realizada com sucesso!'
-                }
-                if (data.IsOk == false) {
-                    statustitle.innerHTML = 'Ops! Ocorreu um problema!'
                     statustitle.innerHTML = data.message
+                    if (data.codebar !== undefined) {
+                        statustitle.innerHTML = 'Pagamento em Boleto realizado com sucesso!'
+                        statuscodebar.innerHTML = data.codebar
+                    }
+                    return
                 }
-                if (data.codebar !== undefined) {
-                    statuscodebar.innerHTML = data.codebar
+                if (data.Error.IsOk == false) {
+                    statustitle.innerHTML = data.Error.message
+                    return
                 }
 
             }
@@ -136,7 +146,7 @@
     }
 
     function typePayment(el) {
-        typePayment = el.target.getAttribute('data-value')
+        typePay = el.target.getAttribute('data-value')
     }
 
 })();
@@ -168,13 +178,10 @@
         // quando for post precisa passar o header
         ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
         // ajax.send('username=keanu&age=54')
-        ajax.send(`cpf_buyer=${cpf_buyer}&name_buyer=${name_buyer}&email_buyer=${email_buyer}`)
+        ajax.send(`cpf_buyer=${cpf_buyer.value}&name_buyer=${name_buyer.value}&email_buyer=${email_buyer.value}`)
         ajax.onreadystatechange = function () {
             if (ajax.readyState == 4) {
                 var data = JSON.parse(ajax.response)
-                console.log('Resposta: ' + data)
-                console.log('Resposta: ' + data.IsOk)
-                console.log('Resposta: ' + data.codebar)
                 statusbox.classList.add('-active')
                 if (data.IsOk == true) {
                     statustitle.innerHTML = data.message
@@ -190,4 +197,91 @@
             }
         }
     }
+})();
+
+(function () {
+
+    var cpf = document.querySelector('#status_cpf')
+    var box_result = document.querySelector('.box.status .info_status .box_result')
+    var statustitle = document.querySelector('#textStatus')
+    var statusbox = document.querySelector('.statusResponse')
+
+    function listener(event, element, callback) {
+        document.querySelector(element)
+            .addEventListener(event, callback, false)
+    }
+
+    listener('click', '#verifyStatus', verifyStatus)
+
+    function verifyStatus(){
+
+        box_result.innerHTML = ''
+
+        var ajax = new XMLHttpRequest()
+        ajax.open('GET', `http://localhost:8002/payment/status/${cpf.value}`)
+        ajax.send()
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState == 4) {
+                var data = JSON.parse(ajax.response)
+
+                console.log(ajax.response )
+
+                if (data.Error) {
+                    statusbox.classList.add('-active')
+                    statustitle.innerHTML = data.Error.message
+                    return
+                }
+
+                for ( var payment in data.Payment_Information ){
+
+                    if (data.Payment_Information[payment].payment_status === 0) {
+                        data.Payment_Information[payment].payment_status = "Aguardando"
+                    }
+                    if (data.Payment_Information[payment].payment_status === 1) {
+                        data.Payment_Information[payment].payment_status = 'Aprovado'
+                    }
+                    if (data.Payment_Information[payment].payment_status === 2) {
+                        data.Payment_Information[payment].payment_status = 'Negado'
+                    }
+
+                    if (data.Payment_Information[payment].type_payment === 'card') {
+                        box_result.innerHTML +=
+                            `<div class="item">
+                            <div class="line">Id do cliente: ${data.Payment_Information[payment].id_client}</div>
+                            <div class="line -white">Nome do Comprador: ${data.Payment_Information[payment].name_buyer}</div>
+                            <div class="line">Email do Comprador: ${data.Payment_Information[payment].email_buyer}</div>
+                            <div class="line -white">CPF do Comprador: ${data.Payment_Information[payment].cpf_buyer}</div>
+                            <div class="line">Valor do Pagamento: ${data.Payment_Information[payment].amount_payment}</div>
+                            <div class="line -white">Tipo de Pagamento: ${data.Payment_Information[payment].type_payment}</div>
+                            <div class="line -white">Nome do Cartão: ${data.Payment_Information[payment].card_information.card_name}</div>
+                            <div class="line">Expiração do Cartão: ${data.Payment_Information[payment].card_information.card_expiration}</div>
+                            <div class="line -white">Numero do Cartão: ${data.Payment_Information[payment].card_information.card_number}</div>
+                            <div class="line">Bandeira do Cartão: ${data.Payment_Information[payment].card_information.card_issuer}</div>
+                            <div class="line -white">Código de Segurança: ${data.Payment_Information[payment].card_information.card_cvv}</div>
+                            <div class="line">Status do Pagamento: ${data.Payment_Information[payment].payment_status}</div>
+                            </div>`
+                        }
+
+
+                    if (data.Payment_Information[payment].type_payment === 'boleto') {
+                        box_result.innerHTML +=
+                            `<div class="item">
+                            <div class="line">Id do cliente: ${data.Payment_Information[payment].id_client}</div>
+                            <div class="line -white">Nome do Comprador: ${data.Payment_Information[payment].name_buyer}</div>
+                            <div class="line">Email do Comprador: ${data.Payment_Information[payment].email_buyer}</div>
+                            <div class="line -white">CPF do Comprador: ${data.Payment_Information[payment].cpf_buyer}</div>
+                            <div class="line">Valor do Pagamento: ${data.Payment_Information[payment].amount_payment}</div>
+                            <div class="line -white">Tipo de Pagamento: ${data.Payment_Information[payment].type_payment}</div>
+                            <div class="line">Código do Boleto: ${data.Payment_Information[payment].boleto_information.boleto_codebar}</div>
+                            <div class="line">Status do Pagamento: ${data.Payment_Information[payment].payment_status}</div>
+                            </div>`
+                        }
+
+
+
+                    }
+            }
+        }
+    }
+
 })();
